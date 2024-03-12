dd54c6a56dc3764ed55d144bfebd2c2a6f6b5cd4
Magee Mooney Caliguire <mageemooney@fastmail.com>
2024-03-11 17:22:44 -0700

    [debug] change next.config.js settings to fix deploy error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/*
+    Added following module.exports instead of commented code on 14-15
+    to handle error when deploying to vercel
+    Error: Collecting page data for undefined is still timing out after 2 attempts.
+        See more info here https://nextjs.org/docs/messages/page-data-collection-timeout
+*/
 
-module.exports = nextConfig;
+module.exports = {
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 1000,
+};
+
+// const nextConfig = {};
+// module.exports = nextConfig;
