95c9eee46bfa8735e6b447b4179168adc0a6193a
Magee Mooney Caliguire <mageemooney@fastmail.com>
2024-03-13 13:15:25 -0700

    [debug] try different ts.config settings to fix Vercel deploy probs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 1000,
+};
